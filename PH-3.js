fe4310156f8d87df8940571499130a659b949561
Murad Hasan <muradhasanshuco00@gmail.com>
2026-02-07 03:39:52 +0600

    3 task complete

--- PH-3.js
+++ PH-3.js
@@ -1,22 +1,20 @@
-// // /*function signature/sample */
-// // function newPrice(currentPrice, discount) {
+  /*function signature/sample */
+  function newPrice(currentPrice, discount) {
 
-// //     let isValid = (typeof currentPrice === 'number' && typeof discount === 'number' && discount >= 0 && discount < 100);
+      let isValid = (typeof currentPrice === 'number' && typeof discount === 'number' && discount >= 0 && discount < 100);
 
-// //     if (!isValid) {
-// //         return "Invalid"
-// //     } else {
-// //         let cal = (currentPrice * discount) / 100;
-// //         let result = currentPrice - cal;
-// //         return result.toFixed(3);
-// //     }
-// // }
+      if (!isValid) {
+          return "Invalid"
+      } else {
+          let cal = (currentPrice * discount) / 100;
+          let result = currentPrice - cal;
+          return result.toFixed(3);
+      }
+  }
 
-// // let result = newPrice(2000, 15)
-// // // output 1200 
-// // console.log(result)
+  let result = newPrice(2000, 15)
 
-
+  console.log(result)
 
 
 
@@ -24,20 +22,22 @@
 
 
 
-// /*function signature/sample */
-// function validOtp(otp) {
-//     let isOk = (typeof otp == 'string' && otp.length <= 9 )
-//     if (!isOk) {
-//         return "Invalid"
-//     } else if ( otp.startsWith("ph-") == false || otp.length < 8) {
-//         return false
-//     }else{
-//         return true
-//     }
-// }
 
-// let re = validOtp("ph-12345")
-// console.log(re)
+
+ /*function signature/sample */
+ function validOtp(otp) {
+     let isOk = (typeof otp == 'string' && otp.length <= 9 )
+     if (!isOk) {
+         return "Invalid"
+     } else if ( otp.startsWith("ph-") == false || otp.length < 8) {
+         return false
+     }else{
+         return true
+     }
+ }
+
+ let re = validOtp("ph-12345")
+ console.log(re)
 
 
 
