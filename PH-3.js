912e07b671f3e171bab92319faf92023c3ee3d37
Murad Hasan <muradhasanshuco00@gmail.com>
2026-02-07 03:37:32 +0600

    first commit

---
+++ PH-3.js
@@ -0,0 +1,65 @@
+// // /*function signature/sample */
+// // function newPrice(currentPrice, discount) {
+
+// //     let isValid = (typeof currentPrice === 'number' && typeof discount === 'number' && discount >= 0 && discount < 100);
+
+// //     if (!isValid) {
+// //         return "Invalid"
+// //     } else {
+// //         let cal = (currentPrice * discount) / 100;
+// //         let result = currentPrice - cal;
+// //         return result.toFixed(3);
+// //     }
+// // }
+
+// // let result = newPrice(2000, 15)
+// // // output 1200 
+// // console.log(result)
+
+
+
+
+
+
+
+
+
+// /*function signature/sample */
+// function validOtp(otp) {
+//     let isOk = (typeof otp == 'string' && otp.length <= 9 )
+//     if (!isOk) {
+//         return "Invalid"
+//     } else if ( otp.startsWith("ph-") == false || otp.length < 8) {
+//         return false
+//     }else{
+//         return true
+//     }
+// }
+
+// let re = validOtp("ph-12345")
+// console.log(re)
+
+
+
+
+
+
+
+/*function signature/sample */
+function finalScore(omr) {
+    let add = omr.wrong + omr.right + omr.skip
+    let rightAnsMark = omr.right * 1
+    let worngMark = omr.wrong * 0.5
+    let resultIs = rightAnsMark - worngMark
+
+    let con = (typeof omr == 'object' && add == 100)
+    if (!con) {
+        return "Invalid"
+    } else {
+        return Math.round(resultIs)
+    }
+}
+
+// let omrResult = finalScore("22")
+let omrResult = finalScore({ right: 80, wrong: 25, skip: 0 })
+console.log(omrResult)
---
+++ test.js
@@ -0,0 +1,7 @@
+ let a = 99
+
+ if ( a >= 0  && a < 100){
+    console.log("ok")
+ }else(
+    console.log("not ")
+ )
